a011a4bffb9c6b7e76cd4825432d4fb80dba3d9b
BlazerHeat <vghoghari82@gmail.com>
2021-04-17 10:11:40 +0530

    make it better

--- client/src/components/Homepage/index.jsx
+++ client/src/components/Homepage/index.jsx
@@ -2,19 +2,15 @@ import { HeroBackground, HeroForeground } from "./Homepage.modules";
 import Typical from "react-typical";
 
 const stepsTime = 2000;
-const steps = [
-  "Full-Stack Developer {/}",
-  stepsTime,
-  "Casual Gamer 🎮",
-  stepsTime,
-];
+const steps = ["Full-Stack Developer", stepsTime, "Casual Gamer 🎮", stepsTime];
 
 function Homepage() {
   return (
     <HeroBackground name="home">
       <HeroForeground>
         <h1>
-          Hello, <br />I am Vatsal Ghoghari
+          Hello,
+          <br />I am Vatsal Ghoghari
         </h1>
         <Typical steps={steps} loop={Infinity} wrapper={"p"} />
       </HeroForeground>

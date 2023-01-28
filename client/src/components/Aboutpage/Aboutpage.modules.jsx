81e79e48a2b20c7eee82a91e6a6fd825667dcbed
BlazerHeat <vghoghari82@gmail.com>
2023-01-28 23:21:15 +0530

    Updated profile pic

--- client/src/components/Aboutpage/Aboutpage.modules.jsx
+++ client/src/components/Aboutpage/Aboutpage.modules.jsx
@@ -1,136 +1,119 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Background = styled.section`
-  width: 100%;
+    width: 100%;
 
-  background: #f5f5f5;
+    background: #f5f5f5;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 const AboutMePage = styled.section`
-  width: 90%;
-  max-width: 1200px;
-  margin: 2em 0;
-  padding: 2em 1em;
+    width: 90%;
+    max-width: 1200px;
+    margin: 2em 0;
+    padding: 2em 1em;
 
-  background: white;
-  box-shadow: 0 1px 1px 0 rgb(0 0 0 / 6%), 0 2px 5px 0 rgb(0 0 0 / 20%);
+    background: white;
+    box-shadow: 0 1px 1px 0 rgb(0 0 0 / 6%), 0 2px 5px 0 rgb(0 0 0 / 20%);
 
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  column-gap: 1.5em;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 1.5em;
 
-  @media (max-width: 900px) {
-    grid-template-rows: 1fr;
-    grid-template-columns: none;
+    @media (max-width: 900px) {
+        grid-template-rows: 1fr;
+        grid-template-columns: none;
 
-    row-gap: 1.5em;
-  }
+        row-gap: 1.5em;
+    }
 `;
 
 const Skills = styled.div`
-  display: grid;
-  grid-template-rows: auto;
+    display: grid;
+    grid-template-rows: auto;
 
-  justify-content: center;
-  align-items: center;
+    justify-content: center;
+    align-items: center;
 
-  row-gap: 1em;
+    row-gap: 1em;
 `;
 
 const MainInfo = styled.div`
-  display: grid;
-  grid-template-columns: auto auto;
+    display: grid;
+    grid-template-columns: auto auto;
 
-  justify-content: center;
-  align-items: center;
+    justify-content: center;
+    align-items: center;
 
-  column-gap: 1em;
+    column-gap: 1em;
 
-  img {
-    width: 50%;
-    min-width: 200px;
-    border-radius: 50%;
-    justify-self: center;
+    img {
+        width: 50%;
+        min-width: 200px;
+        border-radius: 50%;
+        justify-self: center;
 
-    filter: grayscale(100%);
+        padding: 0.5em;
 
-    padding: 0.5em;
-
-    border: 5px solid #0078ff;
-
-    transition: filter 0.25s ease;
-
-    :hover {
-      filter: grayscale(0%);
+        border: 5px solid #0078ff;
+        transform: rotate(15deg);
     }
-  }
 
-  @media (max-width: 900px) {
-    grid-template-rows: 1fr;
-    grid-template-columns: none;
+    @media (max-width: 900px) {
+        grid-template-rows: 1fr;
+        grid-template-columns: none;
 
-    div {
-      justify-self: center;
+        div {
+            justify-self: center;
+        }
     }
-  }
 `;
 
 const SkillGraphs = styled.div`
-  align-self: flex-start;
+    align-self: flex-start;
 
-  h2 {
-    margin-bottom: 0.5em;
-  }
+    h2 {
+        margin-bottom: 0.5em;
+    }
 `;
 
 const ProgressBar = styled.div`
-  width: 100%;
-  height: 0.7rem;
-  margin: 0.5rem 0 1.2rem 0;
+    width: 100%;
+    height: 0.7rem;
+    margin: 0.5rem 0 1.2rem 0;
 
-  background-color: #cde1f8;
+    background-color: #cde1f8;
 `;
 
 const Progress = styled.div`
-  width: ${(props) => props.progress + "%"};
-  height: 100%;
+    width: ${(props) => props.progress + '%'};
+    height: 100%;
 
-  background-color: #0078ff;
+    background-color: #0078ff;
 `;
 
 const AboutMe = styled.div`
-  span {
-    border-bottom: #0078ff 5px solid;
-    font-size: 2em;
-    font-weight: bold;
-  }
+    span {
+        border-bottom: #0078ff 5px solid;
+        font-size: 2em;
+        font-weight: bold;
+    }
 
-  p {
-    margin-top: 2em;
-    line-height: 2em;
-    word-spacing: 0.3em;
-  }
+    p {
+        margin-top: 2em;
+        line-height: 2em;
+        word-spacing: 0.3em;
+    }
 `;
 
 const AboutInfo = styled.div`
-  p {
-    margin-top: 1em;
-    margin-right: 1em;
-  }
+    p {
+        margin-top: 1em;
+        margin-right: 1em;
+    }
 `;
 
-export {
-  Background,
-  AboutMePage,
-  Skills,
-  AboutMe,
-  AboutInfo,
-  MainInfo,
-  SkillGraphs,
-  ProgressBar,
-  Progress,
-};
+export { Background, AboutMePage, Skills, AboutMe, AboutInfo, MainInfo, SkillGraphs, ProgressBar, Progress };
